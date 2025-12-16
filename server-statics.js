33ce00977ed121ed501e6fc5f005a2af68dd061e
fullpropt <beachhtadhgdh00@hotmail.com>
2025-12-16 15:27:08 -0300

    Fix Railway deployment: use Node.js static server instead of Python

---
+++ server-statics.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const distPath = path.join(__dirname, 'dist/spa');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(distPath, req.url === '/' ? 'index.html' : req.url);
+  
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+  
+  switch (extname) {
+    case '.js': contentType = 'text/javascript'; break;
+    case '.css': contentType = 'text/css'; break;
+    case '.json': contentType = 'application/json'; break;
+  }
+  
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile(path.join(distPath, 'index.html'), (err, content) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
